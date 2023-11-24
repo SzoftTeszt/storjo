b12e59c09dfd344b55892e972073b4bc81b8cdeb
AttilaJager <jagerattila@gmail.com>
2023-11-24 17:37:23 +0100

    initial commit

---
+++ src/app/environments.ts
@@ -0,0 +1,12 @@
+export const Environments ={ 
+production:false,
+firebaseConfig : {
+    apiKey: "AIzaSyAXi-vyABOewbv-954ckhrU5SsdmFB8MdY",
+    authDomain: "csakugy-2d0f4.firebaseapp.com",
+    databaseURL: "https://csakugy-2d0f4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "csakugy-2d0f4",
+    storageBucket: "csakugy-2d0f4.appspot.com",
+    messagingSenderId: "615757514429",
+    appId: "1:615757514429:web:f384b854dffbfa592b59d4"
+  }
+}
